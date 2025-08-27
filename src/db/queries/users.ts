36890f0c618dc2f393f6cd2ea1598701e9289dec
Anthony <blacknebula236@gmail.com>
2025-08-27 20:09:32 +0930

    refactor delete users

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -11,8 +11,5 @@ export async function createUser(user: NewUser) {
 }
 
 export async function deleteUsers() {
-  const [result] = await db
-    .delete(users)
-    .returning();
-  return result;
+  await db.delete(users);
 }
